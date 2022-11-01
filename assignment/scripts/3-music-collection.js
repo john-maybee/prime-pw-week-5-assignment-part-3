e0cea699b6a3596c7d457905891599d490874c10
John Maybee <john.d.maybee@gmail.com>
2022-11-01 13:20:11 -0400

    was able to complete the final required part after some trial. Will tackle other parts later this week

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,6 +10,8 @@ function addToCollection(title, artist, yearPublished) {
 console.log(addToCollection('Mt. Joy', 'Mt. Joy', 2018));
 console.log('This is my music collection: ', collection); // trial
 
+// add 6 albums
+//------------------------------------------------------------------------------------------
 addToCollection('Illinois', 'Sufjan Stevens', 2005);
 addToCollection('Awaken, My Love!', 'Childish Gambino', 2016);
 addToCollection('PUP', 'PUP', 2014);
@@ -19,6 +21,8 @@ addToCollection('Good Thing', 'Leon Bridges', 2018);
 
 console.log('This is my music collection: ', collection);
 
+// showCollection Function:
+//------------------------------------------------------------------------------------------
 function showCollection(collection) {
     for(i = 0; i<collection.length; i++){
         console.log(`${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}`);
@@ -28,4 +32,22 @@ function showCollection(collection) {
 
 showCollection(collection);
 
-// name new function: findByArtist 
+// name new function: findByArtist : take in artist as the string parameter
+//------------------------------------------------------------------------------------------
+function findByArtist(artistSearched){
+    let artistResults = [];
+    for (i = 0; i<collection.length; i++){
+        if (collection[i].artist === artistSearched){
+            console.log(artistResults.push(collection[i]));
+        }
+        // else {
+        //     console.log(`artist not a match`);
+        // }  : this else isn't needed, but can use if we want to see what albums arent by an artist.
+    }
+    return console.log(artistResults);
+}
+
+findByArtist('Childish Gambino');  // multiple in collection
+findByArtist('Enya'); // not in collection item
+findByArtist('Sufjan Stevens'); // one in collection
+
