f4b7a4f8f77889f6c9aecb58b723004acd9a846a
John Maybee <john.d.maybee@gmail.com>
2022-10-31 21:29:53 -0400

    just finished getting the function addToCollection to work

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,10 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    let album = {title, artist, yearPublished}
+    return collection.push(album);
+}
+
+console.log(addToCollection('Mt. Joy', 'Mt. Joy', 2018));
+console.log('This is my music collection: ', collection);
